b3d769695877f8c236bb3f73ed028ad7544bbda4
duduzx02 <duduzx02@gmail.com>
2023-03-02 13:11:11 -0300

    Tipos primitivos parte 01

--- MaratonaJava/src/devdojo/maratonaJava/introducao/Aula001OlaMundo.java
+++ MaratonaJava/src/devdojo/maratonaJava/introducao/Aula001OlaMundo.java
@@ -1,4 +1,6 @@
-public class OlaMundo {
+package devdojo.maratonaJava.introducao;
+
+public class Aula001OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
     }
---
+++ MaratonaJava/src/devdojo/maratonaJava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,8 @@
+package devdojo.maratonaJava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 26;
+        System.out.println("A idade é: " + age + " anos!");
+    }
+}
